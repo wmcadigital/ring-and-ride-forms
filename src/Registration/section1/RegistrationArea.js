5131d845a334004c32142cfe2af478ab1a4280cd
Nathan Rogan <rolorogan@gmail.com>
2022-12-07 11:39:36 +0000

    add area selection to registration form

---
+++ src/Registration/section1/RegistrationArea.js
@@ -0,0 +1,96 @@
+import { useFormState } from "react-final-form";
+
+import FormSection from "../../common/FormSection";
+import RadioGroup from "../../common/RadioGroup";
+import Question from "../../common/Question";
+import RadioButton from "../../common/RadioButton";
+import FieldError from "../../common/FieldError";
+import { required } from "../../common/validation";
+
+const RegistrationArea = () => {
+  const stateApi = useFormState();
+  const formValues = stateApi.values;
+
+  const error = stateApi.submitFailed
+    ? stateApi.errors?.registerForYourself
+    : null;
+
+  // disable continue button if coventry is selected
+  if (formValues.Origin === "Coventry") {
+    const button = document.querySelector('button[type="submit"]');
+    button.disabled = true;
+    button.classList.add("wmrards-btn--disabled");
+
+  } else {
+    const button = document.querySelector('button[type="submit"]');
+    button.disabled = false;
+  }
+
+  return (
+    <FormSection>
+      <Question text="Where do you live?" />
+      <RadioGroup error={error}>
+        <FieldError text={error} />
+        <RadioButton
+          key={1}
+          label="Birmingham"
+          validation={required}
+          value="Birmingham"
+          fieldName="Origin"
+        />
+        <RadioButton
+          key={2}
+          label="Coventry"
+          validation={required}
+          value="Coventry"
+          fieldName="Origin"
+        />
+        {formValues.Origin === "Coventry" ? (
+          <><div className="wmrards-warning-text wmrards-m-b-md">
+            <svg className="wmrards-warning-text__icon">
+              <use xlinkHref="#wmrards-general-warning-triangle" href="#wmrards-general-warning-triangle"></use>
+            </svg>
+            If you are starting your trip in Coventry you need to book using West Midlands On-Demand. <a href='https://www.tfwm.org.uk/plan-your-journey/ways-to-travel/buses-in-the-west-midlands/on-demand-buses-in-the-west-midlands/' title='Find out more about West Midlands On-Demand' target='_blank' rel="noreferrer">Find out more on the Transport For West Midlands Website</a>
+          </div></>
+        ) : null}
+        <RadioButton
+          key={3}
+          label="Dudley"
+          validation={required}
+          value="Dudley"
+          fieldName="Origin"
+        />
+        <RadioButton
+          key={4}
+          label="Sandwell"
+          validation={required}
+          value="Sandwell"
+          fieldName="Origin"
+        />
+        <RadioButton
+          key={5}
+          label="Solihull"
+          validation={required}
+          value="Solihull"
+          fieldName="Origin"
+        />
+        <RadioButton
+          key={6}
+          label="Walsall"
+          validation={required}
+          value="Walsall"
+          fieldName="Origin"
+        />
+        <RadioButton
+          key={7}
+          label="Wolverhampton"
+          validation={required}
+          value="Wolverhampton"
+          fieldName="Origin"
+        />
+      </RadioGroup>
+    </FormSection>
+  );
+};
+
+export default RegistrationArea;
