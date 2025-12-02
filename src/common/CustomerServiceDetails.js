fc6c9200b7e02bf70b0e7e861bdb795ca9890e37
Nathan Rogan <nathan.rogan@wmca.org.uk>
2025-12-02 08:29:11 +0000

    customer services details amend

--- src/common/CustomerServiceDetails.js
+++ src/common/CustomerServiceDetails.js
@@ -3,8 +3,9 @@ import ContactDetails from "./ContactDetails";
 const CustomerServiceDetails = () => (
   <ContactDetails>
     <div style={{ fontWeight: "bold" }}>Customer Services</div>
-    <div className="wmnds-m-t-md">Phone: 0330 053 8141</div>
-    <div className="wmnds-m-t-md">Monday to Saturday, 9.30am to 2.30pm</div>
+    <div className="wmnds-m-t-md">Phone: 0345 034 8670</div>
+    <div className="wmnds-m-t-md">Monday to Friday, 8:00AM to 6:00PM</div>
+    <div className="wmnds-m-t-md">Saturday, 8:00AM to 4:00PM</div>
     <div>Sundays and Bank Holidays, Closed</div>
   </ContactDetails>
 );
